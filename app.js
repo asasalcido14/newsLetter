6c0a34b906abae53312d5ba8fba6c23c53342e3b
asasalcido14 <asasalcido14@email.arizona.edu>
2021-04-21 16:38:52 -0700

    completing the log within the post route so we see fname lname and email

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const bodyParser = require("body-parser");
 const app = express();
 // new instance of express
 
+app.use(express.static("public"));
+// provides the path to the static files
+app.use(bodyParser.urlencoded({extended: true}));
+// easier
+
+
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/signUp.html");
 })
+
+app.post ("/", function (req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  console.log(firstName, lastName, email);
+
+});
 
 app.listen(3000, function(){
   console.log("server is running on port 3000");
-})
+});
