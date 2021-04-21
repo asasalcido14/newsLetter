95a86a20fa541765b55386776d6e804bf0f1bd3f
asasalcido14 <asasalcido14@email.arizona.edu>
2021-04-21 15:49:16 -0700

    adding bootstrap components, cdn, and link along w/ styles.css

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
+// new instance of express
 
 app.get("/", function(req, res){
   res.send("server is up and running.")
